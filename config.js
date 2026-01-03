25720e04ee2c8522e1dcecbb8e8fcf122c22b4a4
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-01-03 12:05:02 -0600

    Feat: Add config file

---
+++ config.js
@@ -0,0 +1,3 @@
+export const GITHUB_URL = 'https://github.com/SolorzanoE'
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/szoss/'
+export const EMAIL_ADDRESS = 'eduardo.solorzano.dev@gmail.com'
