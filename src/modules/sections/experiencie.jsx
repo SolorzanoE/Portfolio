c30274ab6724852b8d517946bd2f8f215e02dce7
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-01-04 22:09:19 -0600

    Feat: Add section experiencie

---
+++ src/modules/sections/experiencie.jsx
@@ -0,0 +1,13 @@
+import { Stack, Typography } from "@mui/material"
+
+function Experiencie() {
+  return (
+    <Stack>
+      <Typography> Experiencia </Typography>
+      <Typography> Mi Trayectoria Profesional </Typography>
+      
+    </Stack>
+  )
+}
+
+export default Experiencie
