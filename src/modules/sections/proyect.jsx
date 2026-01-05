bc7448338cdadb6ef7f956912c1922dc8d4763f0
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-01-04 22:09:45 -0600

    Feat: Add section proyect

---
+++ src/modules/sections/proyect.jsx
@@ -0,0 +1,12 @@
+import { Stack, Typography } from "@mui/material"
+
+function Proyect() {
+  return (
+    <Stack>
+      <Typography> Proyectos </Typography>
+      <Typography> Trabajos Realizados </Typography>
+    </Stack>
+  )
+}
+
+export default Proyect
