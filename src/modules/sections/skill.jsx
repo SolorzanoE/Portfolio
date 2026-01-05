f249dff43f30f15b282bbc966671ca1c9a7884c8
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-01-04 22:14:12 -0600

    Feat: Add section skill

---
+++ src/modules/sections/skill.jsx
@@ -0,0 +1,12 @@
+import { Stack, Typography } from "@mui/material"
+
+function Skill() {
+  return (
+    <Stack>
+      <Typography> Habilidades </Typography>
+      <Typography> ... </Typography>
+    </Stack>
+  )
+}
+
+export default Skill
