98a9a9f3abf14913b1b77cb06f2c7ba23dc95353
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-01-04 22:14:39 -0600

    Feat: Add section technology

---
+++ src/modules/sections/technology.jsx
@@ -0,0 +1,11 @@
+import { Stack, Typography } from "@mui/material"
+
+function Technology() {
+  return (
+    <Stack>
+      <Typography> Tecnologías </Typography>
+    </Stack>
+  )
+}
+
+export default Technology
