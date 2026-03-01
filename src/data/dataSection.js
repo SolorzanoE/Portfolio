58da2d736b27e78b8fcd01ed3594eb4bcaf0539f
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-03-01 15:14:24 -0600

    Feat: Add data for management sections

---
+++ src/data/dataSection.js
@@ -0,0 +1,22 @@
+const dataSection = {
+  home: {
+    id: "home",
+    name: "Inicio"
+  },
+  aboutMe: {
+    id: "about-me",
+    name: "Sobre Mi"
+  },
+  experience: {
+    id: "experience",
+    name: "Experiencia"
+  },
+  project: {
+    id: "project",
+    name: "Proyectos"
+  },
+  technology: {
+    id: "technology",
+    name: "Tecnologías"
+  }
+}
